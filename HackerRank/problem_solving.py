ddbd74c07e7180940db8da97104a9a75de6e86ac
Volodymyr <boba.mockob@gmail.com>
2020-01-13 14:10:14 -0500

    ctci-comparator-sorting problem

--- HackerRank/problem_solving.py
+++ HackerRank/problem_solving.py
@@ -293,3 +293,36 @@ case_3_1 = 'APZNC'
 print(abbreviation(case_3_0, case_3_1))
 ###################
 
+# https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem
+
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return f"Player: name = {self.name}, score = {self.score }"
+
+    def comparator(a, b):
+        if a.score == b.score:
+            if a.name == b.name:
+                return 0
+            elif a.name < b.name:
+                return - 1
+            else:
+                return 1
+        else:
+            if a.score > b.score:
+                return - 1
+            else:
+                return 1
+
+
+data = [Player('amy', 100), Player('david', 100), Player('heraldo', 50), Player('aakansha', 75), Player('aleksa', 150)]
+data = sorted(data, key=cmp_to_key(Player.comparator))
+print(data)
+
+################
