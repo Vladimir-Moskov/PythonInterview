703b46d54c8417d1e9342d4534ad9ab5cc22b54d
Volodymyr <boba.mockob@gmail.com>
2020-02-21 10:38:43 -0500

    add PracticeAlgorithmsGreedyCandies Candies

--- HackerRank/problem_solving.py
+++ HackerRank/problem_solving.py
@@ -441,3 +441,30 @@ def flippingBits(n):
     return result
 
 ##############################################################
+# PracticeAlgorithmsGreedyCandies
+# Candies
+# https://www.hackerrank.com/challenges/candies/copy-from/143930711
+
+def candies(n, arr):
+    minArr1 = [1 for i in range(n)]
+    minArr2 = [1 for i in range(n)]
+    finalArr = []
+
+    for i in range(1,len(arr)):
+        if arr[i] > arr[i-1]:
+            minArr1[i]=minArr1[i-1]+1
+        else:
+            minArr1[i] = 1
+
+    for j in range(len(arr)-2,-1,-1):
+        if arr[j] > arr[j+1]:
+            minArr2[j] = minArr2[j+1] +1
+        else:
+            minArr2[j] = 1
+
+    for i in range(len(arr)):
+        finalArr.append(max(minArr1[i], minArr2[i]))
+
+    return sum(finalArr)
+
+############################
