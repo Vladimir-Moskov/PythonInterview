2e15a4c649bbee64c0dedb682c895aff248ee7e0
Vladimir-Moskov <boba.mockob@gmail.com>
2020-03-31 20:40:50 -0400

    decorators-2-name-directory

--- HackerRank/pythonPractice.py
+++ HackerRank/pythonPractice.py
@@ -222,4 +222,27 @@ expression = input()
 result = eval(expression) == k
 print(result)
 
+####################################################################
+# Decorators 2 - Name Directory
+# https://www.hackerrank.com/challenges/decorators-2-name-directory/problem
+
+import operator
+
+
+def person_lister(f):
+    def inner(people):
+        people = sorted(people, key=lambda person: int(person[2]))
+        return map(f, people)
+
+    return inner
+
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
 
