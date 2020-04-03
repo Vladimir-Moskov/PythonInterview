f2f4499afb9f14b2af21a8b22db8dbc2e8bf34a4
Vladimir-Moskov <boba.mockob@gmail.com>
2020-04-03 16:16:15 -0400

    Reduce Function

--- HackerRank/pythonPractice.py
+++ HackerRank/pythonPractice.py
@@ -367,6 +367,19 @@ def ginortS():
     print(input_str)
 
 ############################################################################
+# https://www.hackerrank.com/challenges/reduce-function/problem
+# Reduce Function
 
+from fractions import Fraction
+from functools import reduce
 
+def product(fracs):
+    t = reduce(lambda x, y : x * y, fracs)
+    return t.numerator, t.denominator
 
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
