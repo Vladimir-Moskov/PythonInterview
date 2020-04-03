d4ea2000659ad9693d3bf8ee9eca8f3041482e01
Vladimir-Moskov <boba.mockob@gmail.com>
2020-04-03 16:20:17 -0400

    Validating Email Addresses With a Filter

--- HackerRank/pythonPractice.py
+++ HackerRank/pythonPractice.py
@@ -383,3 +383,38 @@ if __name__ == '__main__':
         fracs.append(Fraction(*map(int, input().split())))
     result = product(fracs)
     print(*result)
+
+############################################################################
+# https://www.hackerrank.com/challenges/validate-list-of-email-address-with-filter/problem
+# Validating Email Addresses With a Filter
+
+
+def fun(email):
+    try:
+        username, url = email.split("@")
+        website, extension = url.split(".")
+    except ValueError:
+        return False
+
+    if not username.replace("-", "").replace("_", "").isalnum():
+        return False
+    if not website.isalnum():
+        return False
+    if len(extension) > 3:
+        return False
+    return True
+
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
