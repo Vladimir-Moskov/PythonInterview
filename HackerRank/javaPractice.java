94f3c1454cf161ad108ccde7e8ed4a9ff5836ae5
Vladimir-Moskov <boba.mockob@gmail.com>
2020-04-13 10:11:29 -0400

    Java Anagrams

--- HackerRank/javaPractice.java
+++ HackerRank/javaPractice.java
@@ -148,3 +148,24 @@ static boolean isAnagram(String a, String b) {
         return true;
 
     }
+
+    // the best good one -
+     static boolean isAnagram(String a, String b) {
+        // Complete the function
+
+        if(a.length() != b.length())
+            return false;
+        int helper_ar[] = new int[26];
+        a = a.toUpperCase();
+        b = b.toUpperCase();
+        for(int i=0; i<a.length(); i++){
+            helper_ar[a.charAt(i) - 'A']++;
+            helper_ar[b.charAt(i) - 'A']--;
+        }
+        int sum = 0;
+        for(int i =0;i<26; i++){
+            sum += helper_ar[i];
+        }
+        return sum == 0 ? true : false;
+
+    }
