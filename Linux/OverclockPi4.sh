39ab08747e969dab2d3db578b4d3f00129f036c3
Vladimir-Moskov <boba.mockob@gmail.com>
2020-04-22 19:26:00 -0400

    Merging Communities

--- Linux/OverclockPi4.sh
+++ Linux/OverclockPi4.sh
@@ -1,3 +1,6 @@
+# https://magpi.raspberrypi.org/articles/how-to-overclock-raspberry-pi-4
+# https://www.seeedstudio.com/blog/2020/02/12/how-to-safely-overclock-your-raspberry-pi-4-to-2-147ghz/
+
 
 1.open up a terminal window - Control, Alt,T
 
@@ -27,4 +30,7 @@ prees Ctrl - X and save by pressing Y
 10.Type- watch -n1 vcgencmd measure_clock arm
 this will display the current CPU speed in real time.
 
-sysbench --num-threads=8 --test=cpu --cpu-max-prime=1800 run
+sysbench --num-threads=8 --test=cpu --cpu-max-prime=2000 run
+
+#  Checking the default speed of CPU
+cat /sys/devices/system/cpu/cpu0/cpufreq/
--- Linux/SimpleCommands.sh
+++ Linux/SimpleCommands.sh
@@ -70,6 +70,17 @@ man vi
 emacs
 
 ########################################
+# https://www.youtube.com/watch?v=V8EUdia_kOE
+# My 5 Favorite Linux Shell Tricks for SPEEEEEED (and efficiency)
+1:04 - sudo !! - re-run previous command with 'sudo' prepended
+1:53 - ctrl-k, ctrl-u, ctrl-w, ctrl-y - cutting and pasting text in the command line
+3:24 - practical kill/yank example
+4:04 - use 'less +F' to view logfiles, instead of 'tail' (ctrl-c, shift-f, q to quit)
+6:25 - ctrl-x-e - continue editing your current shell line in a text editor (uses $EDITOR)
+7:54 - alt-. - paste previous command's argument (useful for running multiple commands on the same resource)
+9:18 - reset - resets/unborks your terminal
+
+########################################
 # Linux Sysadmin Basics 04 -- Shell Features -- Pipes and Redirection
 # https://www.youtube.com/watch?v=-Z5tCri-QlI&list=PLtK75qxsQaMLZSo7KL-PmiRarU7hrpnwK&index=8
 
