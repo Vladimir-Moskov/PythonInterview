7e05f6dd1697bcc3e04748a797fab5cdbf23eedd
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-10 16:51:46 -0400

    Breadth First Search: Shortest Reach

--- Linux/CompleteContainers/featuresDocker.sh
+++ Linux/CompleteContainers/featuresDocker.sh
@@ -1,3 +1,12 @@
 # from the root directory of your CRA app
 docker run --mount type=bind,source="$(pwd)"/build,target=/usr/share/nginx/html -p 8080:80 nginx
 
+# Volumes
+mkdir docker-volume
+cd docker-volume
+touch index.js Dockerfile
+
+docker build --tag=incrementor .
+docker run incrementor
+
+docker run --env DATA_PATH=/data/num.txt --mount type=volume,src=incrementor-data,target=/data incrementor
