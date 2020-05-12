638830c85d2e93b1e152294784fc6d51683e1a50
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-12 10:32:52 -0400

    Prim's (MST) : Special Subtree

--- Linux/CompleteContainers/featuresDocker.sh
+++ Linux/CompleteContainers/featuresDocker.sh
@@ -20,12 +20,29 @@ cd hugo-example/
 docker run --rm -it --mount type=bind,source="$(pwd)",target=/src -p 1313:1313 -u hugo jguyomard/hugo-builder:0.55 hugo server -w --bind=0.0.0.0
 
 # Networking with Docker
+docker info  -f '{{.OSType}}/{{.Architecture}}'
+    linux/armv7l
+docker pull izone/arm:mongo-3.0.9
+docker build -t izone/arm:mongo-3.0.9 ./mongo/3.0.9/
+
 docker network ls
 docker network create --driver=bridge app-net
 docker run -d --network=app-net -p 27017:27017 --name=db --rm mongo:3
+# docker run -d --network=app-net -p 27017:27017 --name=db --rm izone/arm:mongo-3.0.9
+docker ps
 docker run -it --network=app-net --rm mongo:3 mongo --host db
+# docker run -it --network=app-net --rm  izone/arm:mongo-3.0.9  mongo --host db
+     show dbs
+     use tester
+     db.someCollection.insertOne({x: 1})
+     db.someCollection.insert({x: 1})
+    show collections
+docker top db
 
+npm init -y
+npm install mongodb @hapi/hapi hapi-pino
 npm install mongodb@3.3 # you need to add mongodb to your project
+nano index.js
 docker build --tag=my-app-with-mongo .
 docker run -p 3000:3000 --network=app-net --env MONGO_CONNECTION_STRING=mongodb://db:27017 my-app-with-mongo
 
