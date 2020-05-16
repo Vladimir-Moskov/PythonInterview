08d92ef8ee30f929ee3207bc23fbcd270d212675
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-15 21:37:14 -0400

    Bear and Steady Gene

---
+++ HackerRank/Algorithms_Search.py
@@ -0,0 +1,14 @@
+#########################################################################
+# https://www.hackerrank.com/challenges/red-knights-shortest-path/problem
+# Red Knight's Shortest Path
+
+
+# another old good BFS
+def printShortestPath(n, i_start, j_start, i_end, j_end):
+    pass
+
+
+print(printShortestPath(7, 6, 6, 0, 1))  # 4 UL UL UL L
+print(printShortestPath(6, 5, 1, 0, 5))  # Impossible
+print(printShortestPath(7, 0, 3, 4, 3))  # 2 LR LL
+
