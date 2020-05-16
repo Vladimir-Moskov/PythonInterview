825ec1f53c923b0554085cb9a7733e8beec25c4c
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-16 16:07:18 -0400

    Get Node Value

--- HackerRank/DataStructures_LinkedLists.py
+++ HackerRank/DataStructures_LinkedLists.py
@@ -36,3 +36,50 @@ def compare_lists(llist1, llist2):
             return 0
 
     return 1
+
+##########################################################################################################
+# https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem
+# Merge two sorted linked lists
+
+
+def mergeLists(head1, head2):
+    current_node = None
+    while head1 and head2:
+        prev = current_node
+        if head1.data < head2.data:
+            current_node = head1
+            head1 = head1.next
+        else:
+            current_node = head2
+            head2 = head2.next
+        if prev:
+            prev.next = current_node
+        else:
+            head = current_node
+    if head1:
+        current_node.next = head1
+    else:
+        current_node.next = head2
+    return head
+
+###########################################################################################################
+# https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem
+# Get Node Value
+
+
+def getNode(head, positionFromTail):
+    result = None
+    count = 0
+    current = head
+    while current:
+        current = current.next
+        if not result and count == positionFromTail:
+            result = head
+        elif result:
+            result = result.next
+        else:
+            count += 1
+
+
+    return result.data
+
