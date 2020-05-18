20cabf8d365f899cfba515c6390b6bde64f893bd
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-18 12:19:10 -0400

    Kangaroo

--- HackerRank/Algorithms_Implementation.py
+++ HackerRank/Algorithms_Implementation.py
@@ -12,3 +12,20 @@ def gradingStudents(grades):
     return grades
 
 ############################################################################################
+# https://www.hackerrank.com/challenges/kangaroo/problem
+# Kangaroo
+
+def kangaroo(x1, v1, x2, v2):
+    if x1 > x2 and v1 > v2:
+        x1, x2, v1, v2 = x2, x1, v2, v1
+    if x1 > x2 and v1 > v2:
+        return "NO"
+    if x2 > x1 and v2 > v1:
+        return "NO"
+    if v2 == v1:
+        return "NO"
+    if (x2 - x1) % (v1 - v2) == 0:
+        return "YES"
+    return "NO"
+
+############################################################################################
--- Linux/LinuxSystemMaintenanceAndTroubleshooting/practice.sh
+++ Linux/LinuxSystemMaintenanceAndTroubleshooting/practice.sh
@@ -1,7 +1,28 @@
 # 1. Monitoring and Troubleshooting Linux System Performance
 
+# 1. Identifying System Resource Usage
 cd /proc
+
 ls -lah | less
 ps aux | grep 78
 ls -lah 78 | less
 journalctl _PID=78
+ls -p | grep -v / | column
+
+less cpuinfo
+less cpuinfo | grep processor
+cat uptime
+# or
+uptime
+# supportet filesystem types
+less filesystems
+
+# https://access.redhat.com/solutions/406773
+less meminfo
+free -k
+free -h
+
+# 2. Storage and Network Bandwidth Metrics
+
+
+
