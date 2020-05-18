93fb98315c3cf40b3ec0b862213240d8973b7ab8
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-18 10:32:25 -0400

    Binary Search Tree : Insertion

--- HackerRank/DataStructures_Trees.py
+++ HackerRank/DataStructures_Trees.py
@@ -127,3 +127,52 @@ def postOrder(root):
         print(root.info, end=" ")
 
 ############################################################################################
+# https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem?h_r=next-challenge&h_v=legacy
+# Binary Search Tree : Insertion
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+def preOrder(root):
+    if root == None:
+        return
+    print(root.info, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    # Node is defined as
+    # self.left (the left child of the node)
+    # self.right (the right child of the node)
+    # self.info (the value of the node)
+
+    def insert(self, val, currentNode=None):
+        if not self.root:
+            self.root = Node(val)
+            return
+        if not currentNode:
+            currentNode = self.root
+        if currentNode.info < val:
+            if not currentNode.right:
+                currentNode.right = Node(val)
+            else:
+                self.insert(val, currentNode.right)
+        else:
+            if not currentNode.left:
+                currentNode.left = Node(val)
+            else:
+                self.insert(val, currentNode.left)
+
+############################################################################################
