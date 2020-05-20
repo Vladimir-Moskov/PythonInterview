2b94ed5544b551d9f985b224fc8e74d271667051
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-20 11:07:07 -0400

    Kundu and Tree

--- HackerRank/DataStructures_Disjoint.py
+++ HackerRank/DataStructures_Disjoint.py
@@ -139,3 +139,137 @@ def Super_Maximum_Cost_Queries():
             wr = weights[bisect_right(weights, r)-1]
             wl = weights[bisect_right(weights, l-1)-1]
             yield paths[wr]-paths[wl]
+
+###################################################################
+# https://www.hackerrank.com/challenges/kundu-and-tree/problem
+# Kundu and Tree
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.childs = {}
+        self.edge = {}
+        self.num_red_path = 0
+        self.level = 0
+
+    def add_child(self, child, color):
+        self.childs[child.data] = child
+        self.edge[child.data] = color
+
+    def calc_r_path(self):
+        for child in self.childs.values():
+            child.level = self.level + 1
+            child.num_red_path = self.num_red_path
+            if self.edge[child.data] == "r":
+                child.num_red_path += 1
+            child.calc_r_path()
+
+
+def solve(n, tree):
+    result = 0
+
+    all_nodes = [Node(x) for x in range(1, n + 1)]
+    all_nodes.insert(0, None)
+    for val in tree:
+        par_v, child_v, color = val
+        parent = all_nodes[par_v]
+        child = all_nodes[child_v]
+        parent.add_child(child, color)
+
+    root = all_nodes[1]
+    root.calc_r_path()
+
+    return result
+
+
+n = 5
+tree = [[1, 2, 'b'], [2, 3, 'r'], [3, 4, 'r'], [4, 5, 'b']]
+print(solve(n, tree))
+
+# import os
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+#
+#     n = int(input())
+#
+#     tree = []
+#
+#     for _ in range(n-1):
+#         raw_inpit = input().rstrip().split()
+#         tree.append([int(raw_inpit[0]), int(raw_inpit[1]), raw_inpit[2]])
+#
+#
+#     result = solve(n, tree)
+#
+#     fptr.write(str(result))
+#     fptr.write('\n')
+#
+#     fptr.close()
+
+
+from decimal import Decimal
+
+
+class DisjointSet:
+    def __init__(self):
+        self.parent = self
+        self.size = 1
+
+    def find_parent(self):
+        if self.parent != self:
+            self.parent = self.parent.find_parent()
+        return self.parent
+
+    def union(self, other):
+        if self == other:
+            return
+        root = self.find_parent()
+        other_root = other.find_parent()
+        if root == other_root:
+            return
+        if root.size > other_root.size:
+            other_root.parent = root
+            root.size += other_root.size
+        else:
+            root.parent = other_root
+            other_root.size += root.size
+
+
+def nc2(n):
+    if n < 2:
+        return 0
+    return Decimal(n*(n-1)/2)
+
+
+def nc3(n):
+    if n < 3:
+        return 0
+    return Decimal(n*(n-1)*(n-2)/6)
+
+
+n = int(input())
+components = [None]*n
+for i in range(n-1):
+    a, b, c = input().split()
+    a = int(a)-1
+    b = int(b)-1
+    if c == 'r':
+        continue
+    if not components[a]:
+        components[a] = DisjointSet()
+    if not components[b]:
+        components[b] = DisjointSet()
+    components[a].union(components[b])
+
+uniqueComponents = set()
+for x in components:
+    if x:
+        uniqueComponents.add(x.findParent())
+valid_triplets = Decimal(nc3(n))
+
+for x in uniqueComponents:
+    valid_triplets -= nc3(x.size)
+    valid_triplets -= nc2(x.size)*(n-x.size)
+print(int(valid_triplets) % (10**9+7))
