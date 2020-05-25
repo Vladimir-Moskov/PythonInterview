e0e0221dfecc270bcdf119cb423b3f333aa6bd4f
Vladimir-Moskov <boba.mockob@gmail.com>
2020-05-25 18:44:10 -0400

    Goodland Electricity

---
+++ HackerRank/Algorithms_Greedy.py
@@ -0,0 +1,26 @@
+# Algorithms - Greedy
+
+##################################################################################
+# https://www.hackerrank.com/challenges/pylons/problem
+# Goodland Electricity
+
+
+def pylons(plant_range, cities_ar):
+    plant_range -= 1
+    result = 0
+    start = 0
+    prev = -1
+    while start < len(cities_ar):
+        next_step = min(start + plant_range, len(cities_ar) - 1)
+        while cities_ar[next_step] != 1 and next_step >= prev:
+            next_step -= 1
+        if next_step == prev:
+            return -1
+        result += 1
+        start = next_step + plant_range + 1
+        prev = next_step
+    return result
+
+print(pylons(3, [0, 1, 1, 1, 0, 0, 0]))  # -1
+print(pylons(2, [0, 1, 1, 1, 1, 0]))  # 2
+##################################################################################
