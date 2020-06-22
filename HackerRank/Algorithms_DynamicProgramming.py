63bf173853c0343c53a892db278804ba4c5c2c9c
Vladimir-Moskov <boba.mockob@gmail.com>
2020-06-21 20:19:01 -0400

    Lego Blocks

--- HackerRank/Algorithms_DynamicProgramming.py
+++ HackerRank/Algorithms_DynamicProgramming.py
@@ -154,8 +154,215 @@ def stockmax(prices):
 
     return result
 
-print(stockmax([1, 2])) # 1
-print(stockmax([2, 1])) # 0
-print(stockmax([1, 3, 1, 2])) # 3
-print(stockmax([5, 3, 2])) # 0
-print(stockmax([1, 2, 100])) # 197
+# print(stockmax([1, 2])) # 1
+# # print(stockmax([2, 1])) # 0
+# # print(stockmax([1, 3, 1, 2])) # 3
+# # print(stockmax([5, 3, 2])) # 0
+# # print(stockmax([1, 2, 100])) # 197
+
+
+##################################################################################################
+# https://www.hackerrank.com/challenges/lego-blocks/problem
+# Lego Blocks
+
+max_width = 4
+
+
+def generate_all(m_width):
+    next_add = [[i] for i in range(1, max_width + 1)]
+    result = []
+    while next_add:
+        next_temp = []
+        for val in next_add:
+            sum_v = sum(val)
+            if sum_v == m_width:
+                result.append(val)
+            else:
+                for i in range(1, max_width + 1):
+                    if sum_v + i > max_width:
+                        break
+                    else:
+                        new_val = val[:]
+                        new_val.append(i)
+                        next_temp.append(new_val)
+        next_add = next_temp
+
+    return result
+
+
+def is_valid(base_ar, top_ar, m_width):
+    distance = [0] * (m_width - 1)
+    current = 0
+    for i in range(len(base_ar) - 1):
+        val = base_ar[i]
+        current += val
+        distance[current - 1] = 1
+    current = 0
+    for i in range(len(top_ar) - 1):
+        val = top_ar[i]
+        current += val
+        if current < (len(distance) + 1) and distance[current - 1] == 1:
+            return False
+
+    return True
+
+
+def ar_to_key(array_val, m_width):
+    result = 0
+    for val in array_val:
+        result += val * (10 ** m_width)
+        m_width -= 1
+    return result
+
+
+def legoBlocks(n_height, m_width):
+    result = 0
+    all_options = generate_all(m_width)
+    valid_dic = {}
+    for i in range(len(all_options)):
+        val_bot = all_options[i]
+        count = 0
+        ref = []
+        for val_top in all_options:
+            if is_valid(val_bot, val_top, m_width):
+                count += 1
+                ref.append(val_bot)
+        valid_dic[ar_to_key(val_bot)] = ref
+
+    for val in all_options:
+        count = 1
+        for i in range(n_height):
+            count = count * 1
+
+
+    return result
+
+
+def legoBlocks(wall_height, wall_width):
+    mod = 10 ** 9 + 7
+    wall_height = wall_height % mod
+    wall_width = wall_width % mod
+    f = []
+    f.append(0)
+    f.append(1)
+    if wall_width > 1:
+        f.append(2)
+    if wall_width > 2:
+        f.append(4)
+    if wall_width > 3:
+        f.append(8)
+
+    if wall_width > 4:
+        for i in range(5, wall_width + 1):  # stop at index wall_width - 1
+            f.append((f[i - 1] + f[i - 2] + f[i - 3] + f[i - 4]) % mod)
+
+    # total_combos = f[-1] ** wall_height
+
+    g = []
+    for i in range(len(f)):
+        g.append(f[i] ** wall_height % mod)
+
+    # print('f')
+    # for _ in range(wall_height): print(f)
+    # print('---')
+    # print('g')
+    # print(g)
+    # print('---')
+
+    h = [0] * (wall_width + 1)
+    h[1] = 1
+
+    for i in range(2, wall_width + 1):
+        h[i] = g[i]
+        for j in range(1, i):
+            h[i] = (h[i] - h[j] * g[i-j]) % mod
+            a=1
+
+    # print('h')
+    # print(h)
+    # print('---')
+
+    return h[-1] % mod
+# print(legoBlocks(2, 2))
+# print(legoBlocks(3, 2))
+print(legoBlocks(2, 3))
+print(legoBlocks(4, 4))
+
+"""
+    import java.util.Scanner;
+    
+    class Solution{
+        static final int mod=1000000007;
+        static int[] f;
+        static int mult(int x, int y){
+        long z=x;
+        z*=y;
+        return (int) (z%mod);
+        }
+        static int pow(int x, int n){
+        int r=1;
+        while(n!=1){
+            if((n&1)==1) r=mult(r,x);
+            x=mult(x,x);
+            n>>=1;
+        }
+        return mult(x,r);
+        }
+        static int add(int x, int y){
+        return (x+y)%mod;
+        }
+        static void computeF(int highM){
+        if(highM<4) highM=4;
+        f=new int[highM+1];
+        f[0]=1;
+        f[1]=1;
+        f[2]=2;
+        f[3]=4;
+        for(int i=4;i<=highM;++i){
+            int x=0;
+            for(int j=1;j<=4;++j){
+            x=add(x,f[i-j]);
+            }
+            f[i]=x;
+        }
+        }
+        static long solve(int n, int m){
+        int[] g=new int[m+1], h=new int[m+1];
+        g[1]=1;
+        h[1]=0;
+        for(int i=2;i<=m;++i){
+            int x=0;
+            for(int j=1;j<i;++j){
+            x=add(x,mult(g[j],g[i-j]));
+            x=add(x,mult(g[j],h[i-j]));
+            }
+            h[i]=x;
+            g[i]=add(pow(f[i],n),mod-h[i]);
+        }
+        return g[m];
+        }
+        static void input(){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int[] an=new int[t], am=new int[t];
+        for(int i=0;i<t;++i){
+            an[i]=sc.nextInt();
+            am[i]=sc.nextInt();
+        }
+        sc.close();	
+        int highM=0;
+        for(int m: am){
+            if(m>highM) highM=m;
+        }
+        computeF(highM);
+        for(int i=0;i<t;++i){
+            System.out.println(solve(an[i],am[i]));	    
+        }
+        }
+        public static void main(String[] args){
+        input();
+        }
+    }
+"""
+
+##################################################################################################
