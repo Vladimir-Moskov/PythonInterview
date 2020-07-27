4d62e912feee56eb1e682a3b490627f8e7a8dbc9
Vladimir-Moskov <boba.mockob@gmail.com>
2020-07-27 12:46:50 -0400

    The Power Sum

--- Companies/Upstack.py
+++ Companies/Upstack.py
@@ -76,3 +76,121 @@ class Event:
         self.duration = duration
         self.end = start + duration
 
+
+##############################################
+# Round 2
+
+
+
+
+
+
+def n_uniq_max(given_list, n):
+    if n > len(given_list):
+        return 0
+
+    val_list = list(set(given_list))
+    val_list.sort()
+
+    result = sum(val_list[-n:])
+
+    return result
+
+
+def n_uniq_max_2(given_list, n):
+    if n > len(given_list):
+        return 0
+
+    val_dic = {key: 0 for key in given_list}
+
+    if n > len(val_dic.keys()):
+        n = len(val_dic.keys())
+
+    max_list = list(val_dic.keys())[:n]
+    current_min = max_list[0]
+    for val in max_list:
+        if current_min > val:
+            current_min = val
+
+    for key in list(val_dic.keys())[n:]:
+        if key > current_min:
+            for i in range(len(max_list)):
+                if max_list[i] == current_min:
+                    max_list[i] = key
+
+            current_min = max_list[0]
+            for val in max_list:
+                if current_min > val:
+                    current_min = val
+
+    result = 0
+    for val in max_list:
+        result += val
+
+    return result
+
+
+import time
+
+def timeit(temed):
+
+    def decorted(*args, **kargs):
+        start_time = time.perf_counter()
+        result = temed(*args, **kargs)
+        print(f"Execution_time = {time.perf_counter() - start_time} ms")
+        return result
+
+    decorted.__name__ = temed.__name__
+    return decorted
+
+
+@timeit
+def fibonachi(n):
+    if n < 2:
+        return n
+
+    result = 1
+    prev = 0
+    current = 1
+    for i in range(2, n + 1):
+        prev, current = current, prev + current
+        result += current
+
+    return result
+
+# print(fibonachi(100))
+
+
+# print(n_uniq_max_2( [1,2,3,7,23,99,1,2,4,1,15,6,4,99], 3))
+
+
+from datetime import date
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        # a class method to create a Person object by birth year.
+
+    @classmethod
+    def fromBirthYear(cls, name, year):
+        return cls(name, date.today().year - year)
+
+        # a static method to check if a Person is adult or not.
+
+    @staticmethod
+    def isAdult(age):
+        return age > 18
+
+
+person1 = Person('mayank', 21)
+person2 = Person.fromBirthYear('mayank', 1996)
+
+print(person1.age)
+print(person2.age)
+
+# print the result
+print(Person.isAdult(22))
+
--- HackerRank/Algorithms_Recursion.py
+++ HackerRank/Algorithms_Recursion.py
@@ -2,117 +2,47 @@
 # https://www.hackerrank.com/challenges/the-power-sum/problem
 # The Power Sum
 #
-# import math
-#
-#
-# def powerSum(X, N):
-#     x_root_n = (math.po(X)) + 1
-#     arr_2 = [0] * x_sqrt
-#     arr_sum = [0] * x_sqrt
-#
-#     for i in range(x_sqrt):
-#         arr_2[i] = i * i
-#
-#     for i in range(0, N):
-#         k = 1
-#         while arr_sum[k] <= X:
-#             arr_sum[k] =
+
+import math
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def n_uniq_max(given_list, n):
-    if n > len(given_list):
-        return 0
-
-    val_list = list(set(given_list))
-    val_list.sort()
-
-    result = sum(val_list[-n:])
-
-    return result
-
-
-def n_uniq_max_2(given_list, n):
-    if n > len(given_list):
-        return 0
-
-    val_dic = {key: 0 for key in given_list}
-
-    if n > len(val_dic.keys()):
-        n = len(val_dic.keys())
-
-    max_list = list(val_dic.keys())[:n]
-    current_min = max_list[0]
-    for val in max_list:
-        if current_min > val:
-            current_min = val
-
-    for key in list(val_dic.keys())[n:]:
-        if key > current_min:
-            for i in range(len(max_list)):
-                if max_list[i] == current_min:
-                    max_list[i] = key
-
-            current_min = max_list[0]
-            for val in max_list:
-                if current_min > val:
-                    current_min = val
-
+def powerSum(X, N):
     result = 0
-    for val in max_list:
-        result += val
+    x_root_n = int(X ** (1 / N))
+    arr_pow_n = [0] * (x_root_n + 1)
+
+    for i in range(x_root_n + 1):
+        arr_pow_n[i] = i ** N
+
+    result = check_val(arr_pow_n, 1, X)
 
     return result
 
-
-import time
-
-def timeit(temed):
-
-    def decorted(*args, **kargs):
-        start_time = time.perf_counter()
-        result = temed(*args, **kargs)
-        print(f"Execution_time = {time.perf_counter() - start_time} ms")
-        return result
-
-    decorted.__name__ = temed.__name__
-    return decorted
-
-
-@timeit
-def fibonachi(n):
-    if n < 2:
-        return n
-
-    result = 1
-    prev = 0
-    current = 1
-    for i in range(2, n + 1):
-        prev, current = current, prev + current
-        result += current
-
+def check_val(arr, cur_index, cur_val):
+    result = 0
+    if cur_index < len(arr):
+        for i in range(cur_index, len(arr)):
+            if arr[i] == cur_val:
+                result += 1
+                break
+            elif arr[cur_index] > cur_val:
+                break
+            else:
+                result += check_val(arr, i + 1, cur_val - arr[i])
     return result
 
-print(fibonachi(100))
 
 
-# print(n_uniq_max_2( [1,2,3,7,23,99,1,2,4,1,15,6,4,99], 3))
+print(powerSum(10, 2))  # 1
+print(powerSum(100, 2))  # 3
+print(powerSum(100, 3))  # 1
+
+######################################################################
+
+
+
+
+
 
 
 
