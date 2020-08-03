a87842a30f2822ddd2e7b07ba9d6827b0138cd37
Vladimir-Moskov <boba.mockob@gmail.com>
2020-08-03 10:18:02 -0400

    The Longest Common Subsequence

--- HackerRank/Algorithms_DynamicProgramming.py
+++ HackerRank/Algorithms_DynamicProgramming.py
@@ -395,6 +395,45 @@ def unboundedKnapsack(k, arr):
 # The Longest Common Subsequence
 
 
+def longestCommonSubsequence(a, b):
+
+
+    matrix_calc = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i-1] == b[j-1]:
+                matrix_calc[i][j] = matrix_calc[i - 1][j - 1] + 1
+            else:
+                matrix_calc[i][j] = max(matrix_calc[i][j - 1], matrix_calc[i - 1][j])
+
+    max_len = matrix_calc[-1][-1]
+    result = []
+    curr = len(a) - 1, len(b) - 1
+    while max_len:
+        if a[curr[0]] == b[curr[1]]:  # match - belongs to common subseq
+            result.append(a[curr[0]])
+            max_len -= 1
+            curr = (curr[0] - 1, curr[1] - 1)
+        else:  # go to the square that has longer subsequence until now
+            if matrix_calc[curr[0]][curr[1] + 1] > matrix_calc[curr[0] + 1][curr[1]]:
+                curr = (curr[0] - 1, curr[1])
+            else:
+                curr = (curr[0], curr[1] - 1)
+
+    return result[::-1]
+
+a = [3, 2, 3, 4, 5]
+b = [3, 3, 7, 8, 9, 5]
+print(longestCommonSubsequence(a, b))  # 3 3 5
+
+a = [1, 2, 3, 4, 1]
+b = [3, 4, 1, 2, 1, 3]
+print(longestCommonSubsequence(a, b))  # 1 2 3
+
+a = [3, 9, 8, 3, 9, 7, 9, 7, 0]
+b = [3, 3, 9, 9, 9, 1, 7, 2, 0, 6]
+print(longestCommonSubsequence(a, b))  # 3 3 9 9 7 0
 
 
 
